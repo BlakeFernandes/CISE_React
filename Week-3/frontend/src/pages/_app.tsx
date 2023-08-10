59c1c00550d5f12b59cb27ae44c429b1bc2ffeb8
BlakeFernandes <blakef148@gmail.com>
2023-08-11 08:35:26 +1200

    bug fixes

--- Week-3/frontend/src/pages/_app.tsx
+++ Week-3/frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { type AppType } from "next/app";
 
 import '~/styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.css';
 
 const MyApp: AppType<{}> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
